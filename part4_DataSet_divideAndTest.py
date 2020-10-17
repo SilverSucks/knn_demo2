47ba967713e375bb50f3f076af2baf9f498b7955
oscarlkl <kangleli.vip@gmail.com>
2020-10-17 12:11:53 +0800

    使用支持向量机对情感分析的例子进行预测

--- part4_DataSet_divideAndTest.py
+++ part4_DataSet_divideAndTest.py
@@ -5,6 +5,7 @@ import numpy as np
 import part1_file2matrix as f2m
 import part3_autoNorm as an
 import KNN_function as K
+from sklearn import svm
 
 # 处理原始数据
 datingDataMat, datingLabels = f2m.file2matrix('E:/class/bilibili机器学习笔记（福州大学江灏）/老师发的课件/MachineLearning/KNN算法实战（二）/datingTestSet.txt')
@@ -28,19 +29,28 @@ print(trainSize)
 testSize = int((1-m)*dataSize)
 print(testSize)
 
+
 # knn算法k值
-k = 5
+# k = 5
 # 预测结果错误的个数
 error = 0
+
+# 使用支持向量机
+model = svm.SVC()
+model.fit(normDataSet[0: trainSize, :], datingLabels[0: trainSize])
+
 
 # 模型预测的错误率
 for i in range(testSize):
     # 前面trainSize个数据不用管， 从trainSize+i-1个开始为测试数据
     # 参数1：测试集， 参数2：训练集， 参数3：标签向量， 参数4：选择距离最小的k个点
-    result = K.knn(normDataSet[trainSize+i-1, :], normDataSet[0: trainSize, :], datingLabels[0: trainSize], k)
+    # result = K.knn(normDataSet[trainSize+i, :], normDataSet[0: trainSize, :], datingLabels[0: trainSize], k)
+
+    # 使用支持向量机 , 这里要注意  需要通过reshape函数把数据进行规整
+    result = model.predict(normDataSet[trainSize+i, :].reshape(1, -1))
 
     # 预测的结果不等于真实结果
-    if result != datingLabels[trainSize+i-1]:
+    if result != datingLabels[trainSize+i]:
         error = error + 1
 # 打印错误率
 print('rate of the error rusult:', error/testSize)
